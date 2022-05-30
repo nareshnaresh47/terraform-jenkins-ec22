b8c7aacb195164c075813259a66dc7a2c6e2deab
Naresh N <nareshdevops47@gmail.com>
2022-05-30 21:04:36 +0530

    updated

---
+++ main.tf
@@ -0,0 +1,11 @@
+provider "aws" {
+    region     = "ap-south-1"
+ }
+
+ resource "aws_instance" "example" {
+   ami           = "ami-04893cdb768d0f9ee"
+   instance_type = "t2.micro"
+   key_name = "docker"
+
+
+ }
