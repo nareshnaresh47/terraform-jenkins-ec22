e290dd6ae6abe1c947870e3eeb931584a8f06b7e
nareshnaresh47 <96810093+nareshnaresh47@users.noreply.github.com>
2022-11-30 08:28:59 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -5,7 +5,7 @@ provider "aws" {
  resource "aws_instance" "example" {
    ami           = "ami-04893cdb768d0f9ee"
    instance_type = "t2.micro"
-   key_name = "dockerdevops"
+   key_name = "aplb"
 
 
  }
